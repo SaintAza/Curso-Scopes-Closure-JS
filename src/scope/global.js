8e9f9ed03c03f51c9c140a25d746ba2ced95a801
Azahel <azahelcaballero@gmail.com>
2020-10-05 11:02:50 -0600

    clase 1

---
+++ src/scope/global.js
@@ -0,0 +1,16 @@
+var hello = 'hello'
+var hello = 'hello +'
+let world = 'world'
+const hw = 'hello world'
+
+
+console.log(hello);
+
+
+function anotherFuction() {
+    console.log(hello);
+    console.log(world);
+    console.log(hw);
+}
+
+anotherFuction
