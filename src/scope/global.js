a5365121d20040338536d22a744a1a9798739452
Azahel <azahelcaballero@gmail.com>
2020-10-05 12:23:37 -0600

    clase 2

--- src/scope/global.js
+++ src/scope/global.js
@@ -13,4 +13,19 @@ function anotherFuction() {
     console.log(hw);
 }
 
-anotherFuction
+anotherFuction();
+
+
+/*
+
+Scopes de:
+
+--var puede sobrescribir la declaración y asignación de una variable y alterar su valor
+---let no permite volver a declarar una variable con el mismo nombre pero permite la asignación de un nuevo valor
+---const tampoco permite reasignar una declaración ni asignación de un nuevo valor
+
+Una variable sin var, let o const, dentro de una función puede ser reconocida como una variable global pero esto es una mala práctica.
+
+
+
+*/
